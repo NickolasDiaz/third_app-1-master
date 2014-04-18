9a8575f168ad06770a7dcb63ea05e5f6ad00546c
Nickolas Diaz <ndiaz@chicagotechacademy.org>
2014-04-18 13:09:26 -0500

    Part 14

--- db/schema.rb
+++ db/schema.rb
@@ -20,4 +20,6 @@ ActiveRecord::Schema.define(:version => 20140414175641) do
     t.datetime "updated_at", :null => false
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+
 end
