694c7ee4fd7bc976f99e98880333c0dd7b750a89
Nickolas Diaz <ndiaz@chicagotechacademy.org>
2014-04-29 12:43:06 -0500

    Beginning of Part 18

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,11 +3,17 @@ require 'spec_helper'
 describe "UserPages" do
 
 	subject { page }
-	
-	describe "signup page" do
-		before { visit signup_path}
 
-		it { should have_selector('h1', text: 'Sign up')}
-		it { should have_selector('title', text: 'Sign up')}
+	describe "signup page" do
+		before { visit signup_path(user) }
+
+		it { should have_selector('h1', 	text: 'Sign up')}
+		it { should have_selector('title',  text: 'Sign up')}
 	end
+
+	describe "profile page" do
+		before { visit user_path}
+
+		it { should have_selector('h1', 	text: user.name)}
+		it { should have_selector('title',  text: user.name)}
 end
